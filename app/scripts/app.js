e69109925dde20ca6d8a393a1f3969b05d8d2328
Robert <fauverism@Roberts-MacBook-Pro.local>
2015-10-01 06:10:42 -0400

    adding router params

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,7 @@
  *
  * Main module of the application.
  */
-angular
-  .module('artApp', [
+angular.module('artApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -17,9 +16,44 @@ angular
     'ngSanitize',
     'ngTouch',
     'ui.bootstrap',
-    'duScroll'
-  ]);
+    'ui.router'
+  ])
 //   .value('$sniffer', { history: false })
-//   .config(function ($locationProvider) {
-//     $locationProvider.html5Mode(true).hashPrefix('!');
-//   });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/appointments");
+    $stateProvider
+      .state('about', {
+        url: "/about",
+        templateUrl: "/views/partials/about.html"
+      })
+      .state('hair-services', {
+        url: "/hair-services",
+        templateUrl: "/views/partials/hair-services.html"
+      })
+      .state('spa-services', {
+        url: "/spa-services",
+        templateUrl: "/views/partials/spa-services.html"
+      })
+      .state('appointments', {
+        url: "/appointments",
+        templateUrl: "/views/partials/appointments.html"
+      })
+      .state('team', {
+        url: "/team",
+        templateUrl: "/views/partials/team/team.html"
+      })
+      .state('social', {
+        url: "/social",
+        templateUrl: "/views/partials/social.html"
+      })
+      .state('products', {
+        url: "/products",
+        templateUrl: "/views/partials/products.html"
+      })
+    ;
+      //Appointments
+      //Social
+      //Team
+      //Products
+      //Contacts
+  });
