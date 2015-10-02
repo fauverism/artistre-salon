f77877338916e34c47eaf9b178b975443ddc62da
Robert <fauverism@Roberts-MBP.hsd1.nj.comcast.net>
2015-10-02 09:26:40 -0400

    adding bios

--- app/scripts/controllers/teamController.js
+++ app/scripts/controllers/teamController.js
@@ -3,13 +3,13 @@
 angular.module('artApp')
   .controller('teamCtrl', function ($scope, $modal, $log) {
 
-    $scope.hoverIn = function() {
-      this.hoverEdit = true;
-    };
-
-    $scope.hoverOut = function() {
-      this.hoverEdit = false;
-    };
+    //$scope.hoverIn = function() {
+    //  this.hoverEdit = true;
+    //};
+    //
+    //$scope.hoverOut = function() {
+    //  this.hoverEdit = false;
+    //};
 
     $scope.animationsEnabled = true;
 
@@ -60,6 +60,90 @@ angular.module('artApp')
       var modalInstance = $modal.open({
         animation: $scope.animationsEnabled,
         templateUrl: 'wendyContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openJami = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'jamiContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openAshley = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'ashleyContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openCinnamon = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'cinnamonContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openAmi = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'amiContent.html',
         controller: 'teamCtrl',
         size: size,
         resolve: {
