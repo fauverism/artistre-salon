5e06346e8c3ebee169cd08dc9464be8fbe3ebde8
Robert <fauverism@Roberts-MacBook-Pro.local>
2015-10-02 05:06:30 -0400

    3 modals down lots more to go

--- app/scripts/controllers/teamController.js
+++ app/scripts/controllers/teamController.js
@@ -13,11 +13,53 @@ angular.module('artApp')
 
     $scope.animationsEnabled = true;
 
-    $scope.open = function (size) {
+    $scope.openAlicia = function (size) {
 
       var modalInstance = $modal.open({
         animation: $scope.animationsEnabled,
-        templateUrl: 'myModalContent.html',
+        templateUrl: 'aliciaContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openLyn = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'lynContent.html',
+        controller: 'teamCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.openWendy = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'wendyContent.html',
         controller: 'teamCtrl',
         size: size,
         resolve: {
